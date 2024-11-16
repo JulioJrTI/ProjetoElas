a58d4dfa387c58e37d8d497a053f657181df8ca2
Julio Alves da Silva Jr <148810548+JulioJrTI@users.noreply.github.com>
2024-11-16 20:46:00 -0300

    Pre-Concept 002

--- ProjetoElas/app/src/main/java/com/example/projetoelas/MainActivity.kt
+++ ProjetoElas/app/src/main/java/com/example/projetoelas/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.projetoelas
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+
+// Bibliotecas referentes a logica de pesquisa do navegador
+import android.content.Intent
+import android.net.Uri
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,7 +27,24 @@ class MainActivity : AppCompatActivity() {
 
     // Logica do aplicativo
 
+    // Caixa de Texto Teste
 
+    // Teste, ao pressionarmos um botão, o conteudo string de um componente de texto será mudado
+    fun MudarTexto(view:View){
+        val textoTeste = findViewById<TextView>(R.id.txt_Teste)
+        textoTeste.setText("Vc clicou no botao!")
+    }
+
+    // Botão Hospital
+    fun Hospital(view: View){
+        val textoTeste = findViewById<TextView>(R.id.txt_Teste)
+        textoTeste.setText("Vc clicou no botao do hospital!")
+
+        val pesquisa = Uri.parse("https://www.google.com/search?q=hospitais")
+        val intent = Intent(Intent.ACTION_VIEW, pesquisa)
+        startActivity(intent)
+
+    }
 
 
 }
